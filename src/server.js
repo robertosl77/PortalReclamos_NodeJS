288fd2e44cb5c07e62ad3afb88c6f70618569c05
robertosl77 <robertosl77@gmail.com>
2025-04-22 23:15:18 -0300

    alpha_v1.1.1

--- src/server.js
+++ src/server.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 8080;
 // Middleware para servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
 
+// Esto le permite a Express leer datos enviados con application/x-www-form-urlencoded, que es lo que hacen los formularios HTML por defecto.
+app.use(express.urlencoded({ extended: true }));
+
 // Ruta por defecto (redirección desde / a /PortalReclamos)
 app.get('/', (req, res) => {
   console.log('Redirigiendo a /PortalReclamos');
@@ -22,6 +25,15 @@ app.get('/', (req, res) => {
 // Ruta principal
 app.get('/PortalReclamos', (req, res) => {
   res.sendFile('login.html', { root: 'public' }); // Retorna el archivo login.html
+});
+
+app.post('/api/login', (req, res) => {
+  const { floatingInput, floatingPassword } = req.body;
+  console.log(`Usuario: ${floatingInput}`);
+  console.log(`Contraseña: ${floatingPassword}`);
+
+  // Más adelante: validación contra LDAP
+  res.send('Login recibido');
 });
 
 // escuchar el puerto
